963f439090a74def6233f738a2c7148708dc087f
Evgeniya Karpunina <karpunina@tehnosk.ru>
2016-11-22 13:48:44 +0300

    Initial commit

---
+++ src/main/java/com/mongoacl/MongoAclApplication.java
@@ -0,0 +1,12 @@
+package com.mongoacl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoAclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoAclApplication.class, args);
+	}
+}
